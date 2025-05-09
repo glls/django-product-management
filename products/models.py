c5e0ffc082446f58af118b78433ec6403a8734de
Kostas Sidiropoulos <koter@DESKTOP-8V3GII9.>
2025-05-09 14:19:25 +0300

    Task-2 complete

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    notes = models.TextField()
+    serial = models.CharField(max_length=100, unique=True)
+    rfid = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
