442ccf592b5c09e2756dab0e5603b5f5641f4c5b
ksidiropoulos90 <con0390@gmail.com>
2025-05-12 17:18:24 +0300

    Add default home page and image in products admin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Category, Product
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -6,9 +7,18 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ["image_preview"]
     list_display = ["name", "category"]
     list_filter = ["name", "category"]
     search_fields = ["name", "category", "serial", "rfid", "code", "price"]
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src={} width="200" style="object-fit:contain;"/>', obj.image.url)
+        return "(No image)"
+    image_preview.short_description = "Current image"
+
+    fields = ["name", "category", "notes", "serial", "rfid", "code", "image", "image_preview", "price"]
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
