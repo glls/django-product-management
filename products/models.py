5d351c9ae6e6ecff3ec7b9995e4e20771855d584
Kostas Sidiropoulos <con0390@gmail.com>
2025-05-14 15:57:48 +0300

    Change field options products

--- products/models.py
+++ products/models.py
@@ -15,11 +15,11 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
-    notes = models.TextField()
+    notes = models.TextField(null=True, blank=True)
     serial = models.CharField(max_length=100, unique=True)
-    rfid = models.CharField(max_length=100)
-    code = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='media')
+    rfid = models.CharField(max_length=100, null=True, blank=True)
+    code = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='media', null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
