7e7209ce59875566dfb1b5f1cf14aa97b17dbd47
Kostas Sidiropoulos <con0390@gmail.com>
2025-05-15 18:06:06 +0300

    Add product image compression

--- products/models.py
+++ products/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from django.core.exceptions import ValidationError
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -14,9 +17,10 @@ class Category(models.Model):
         return self.name
 
 def validate_image_file_size(image):
-    max_size = 100 * 1024  # 100kB max in bytes
+    size_units = 1024 * 1024 # 1MB in bytes
+    max_size = 20 * size_units  # 20MB max
     if image.size > max_size:
-        raise ValidationError(f"Image size should not exceed {max_size / 1024:.2f}kB. Current size: {image.size / 1024:.2f}kB")
+        raise ValidationError(f"Image size should not exceed {max_size / size_units:.2f}MB. Current size: {image.size / size_units:.2f}MB")
     return image
 
 class Product(models.Model):
@@ -31,3 +35,40 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if self.image:
+            self.image.open()  # Ensure file is open
+            self.image.seek(0)  # Reset pointer to start
+            img = Image.open(self.image)
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            # Resize: largest side = 1024px, keep aspect ratio
+            max_size = 1024
+            width, height = img.size
+
+            dimensions = {"width": width, "height": height}
+            largest = max(dimensions, key=dimensions.get)
+
+            #Reduce output dimensions if larger than max_size
+            if dimensions[largest] > max_size:
+                ratio = dimensions[largest] / max_size
+                dimensions[largest] = max_size
+                print(f"dimensions: {dimensions}")
+                if largest != "width":
+                    dimensions["width"] = int(dimensions["width"] / ratio)
+                else:
+                    dimensions["height"] = int(dimensions["height"] / ratio)
+                print(f"dimensions: {dimensions}")
+                img = img.resize((dimensions["width"], dimensions["height"]), Image.LANCZOS)
+
+            # Compress to 85% quality 
+            output = BytesIO()
+            img.save(output, format='JPEG', quality=85)
+            output.seek(0)
+            self.image.save(
+                f"{self.image.name.split('.')[0]}.jpg",
+                ContentFile(output.read()),
+                save=False # Don't save the model yet
+            )
+        super().save(*args, **kwargs) # Call the original save method to save the model
